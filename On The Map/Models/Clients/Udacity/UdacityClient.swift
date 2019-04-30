f26f72776db849d09cb0fd1f3a53bed61c743c23
Baraa Hesham <brunocode7@gmail.com>
2019-04-30 02:57:23 -0700

    Signet button

--- On The Map/Models/Clients/Udacity/UdacityClient.swift
+++ On The Map/Models/Clients/Udacity/UdacityClient.swift
@@ -43,7 +43,6 @@ class UdacityClient{
         let task = session.dataTask(with: request) { data, response, error in
             func sendError(_ error: String,_ errorCode:Int?) {
                 print(error)
-                let userInfo = [NSLocalizedDescriptionKey : error]
                 completionHandlerForPOST(nil, errorCode)
             }
             
@@ -82,5 +81,63 @@ class UdacityClient{
             
         }
         task.resume()
+    }
+    
+    
+    // function to delete and logout from Udacity Api
+    static func taskForDELETEMethod(sessionId:String, completionHandlerForDELETE: @escaping (_ result: Session?, _ errorCode: Int?) -> Void){
+        var request = URLRequest(url: URL(string: "https://onthemap-api.udacity.com/v1/\(sessionId)")!)
+        request.httpMethod = "DELETE"
+        var xsrfCookie: HTTPCookie? = nil
+        let sharedCookieStorage = HTTPCookieStorage.shared
+        for cookie in sharedCookieStorage.cookies! {
+            if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            func sendError(_ error: String,_ errorCode:Int?) {
+                print(error)
+                completionHandlerForDELETE(nil, errorCode)
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                //network connection error
+                sendError("There was an error with your request: \(error!)",nil)
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx! it is = \(String(describing: (response as! HTTPURLResponse).statusCode))",((response as? HTTPURLResponse)?.statusCode)!)
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                sendError("No data was returned by the request!",nil)
+                return
+            }
+            print("this is data = \(String(describing: data)) and this is response = \(String(describing: response))")
+            let range = Range(5..<data.count)
+            
+            let newData = data.subdata(in: range)
+            
+            
+            do {
+                let deleteSession = try JSONDecoder().decode(Session.self,from: newData)
+                print(deleteSession.id)
+                completionHandlerForDELETE(deleteSession,nil)
+                
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }
+        task.resume()
+        
     }
 }
